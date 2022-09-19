1d1c9add76058d8a2b1fb6a47c60850687246e35
Oussama sallak <osallak@student.1337.ma>
2022-09-19 19:50:00 +0100

    day3 finished exept the last exercice

--- day08/day01/ex00/randomChump.cpp
+++
@@ -1,9 +0,0 @@
-#include "Zombie.hpp"
-
-
-
-void randomChump( std::string name )
-{
-	Zombie newZombie = Zombie(name);
-	newZombie.announce();
-}
--- day08/day01/ex01/zombieHorde.cpp
+++
@@ -1,9 +0,0 @@
-#include "Zombie.hpp"
-
-Zombie* zombieHorde( int N, std::string name)
-{
-	Zombie* newZombies = new Zombie[N];
-	while (--N >= 0)
-		newZombies[N].initName(name);
-	return newZombies;
-}
